e796347c62fef9803c0508f21c942f94edf8cbb5
Mikhail Diadchenko <darsa771@gmail.com>
2022-06-13 18:51:15 +0300

    Update custom.js

--- src/custom.js
+++ src/custom.js
@@ -2,7 +2,7 @@ $('.owl-carousel').owlCarousel({
   items: 1,
   loop: true,
   dots: true,
-  // autoplay: true,
-  // autoplayTimeout: 5000,
-  // autoplayHoverPause: true,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
 });
